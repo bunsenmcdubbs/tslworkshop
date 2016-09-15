dee65c52e76de7f03a28f9bcc94f0b3f1ad971bb
Andrew Dai <theman@andrewdai.co>
2016-09-15 15:36:21 -0400

    add html/static files to helloflask

--- helloflask/hello.py
+++ helloflask/hello.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 app = Flask(__name__)
 
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name="Michelle"):
+    return "Hello %s!" % name
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/<path:filepath>")
+def index(filepath="index.html"):
+    return send_from_directory("", filepath)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
