017188e6bf734c59ebc4d9e0538441e5aff78d5f
Andrew Dai <theman@andrewdai.co>
2016-09-14 20:19:24 -0400

    lol

--- todo/views/main.js
+++ todo/views/main.js
@@ -1,15 +1,50 @@
-function renderTodos(todos) {
-  todos.map(function(todo) {
-    return $("<li class='list-group-item todo-item'>" +
-        "<input type='checkbox' checked='" + todo.done === true + "'>" +
-        todo.content + "</li>");
+function renderTodos(tasks) {
+  console.log(tasks);
+  tasks.forEach(function(task) {
+    var $task = $("<li class='list-group-item todo-item" +
+      (task.done ? "done" : "") + "'>" +
+      "<input type='checkbox' " + (task.done ? "checked" : "") + ">" +
+      task.content + "</li>");
+
+    $task.attr('id', task.id);
+
+    $task.find('input[type=checkbox]').click(function(event) {
+      var done = $(this).is(':checked');
+      if (done) {
+        markDone(this.attr('id'));
+      } else {
+        markUndone(this.attr('id'));
+      }
+    });
+
+    $("#todo-items").append($task);
   });
 }
 
 function getTodos() {
   $.get('/tasks', function(data) {
-    console.log(data);
+    renderTodos(data.tasks);
   });
 }
+
+function createTodo(content) {
+  $.post('/tasks', {content: content}, function(data) {
+    renderTodos(data.tasks);
+  });
+}
+
+function markDone(id) {
+  $.post("/tasks/"+id+"/done", function(data) {
+    renderTodos(data.tasks);
+  });
+}
+
+function markUndone(id) {
+  $.post("/tasks/"+id+"/undone", function(data) {
+    renderTodos(data.tasks);
+  });
+}
+
+
 
 getTodos();
