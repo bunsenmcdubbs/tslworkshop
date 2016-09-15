00db7bb51f7acf1bccc0aa08d279d72749519a42
Andrew Dai <theman@andrewdai.co>
2016-09-15 16:06:23 -0400

    add comments

--- todo/server.py
+++ todo/server.py
@@ -1,7 +1,9 @@
 from flask import Flask, json, send_from_directory, request
 app = Flask(__name__)
 
+## global static id to ensure that all our Tasks have unique ids
 taskId = -1
+
 class Task():
     ## this is the contructor
     def __init__(self, content):
@@ -11,11 +13,12 @@ class Task():
         self.content = content
         self.done = False
 
-    ## "to string", makes printing Task objects more human-readable
+    ## "to string", makes printing Task objects more human-readable in your terminal
     ## similar to __str__(), they have subtle differences
     def __repr__(self):
         return '#%i %s' % (self.id, self.content) + (" <done>" if self.done else "")
-    ##
+
+    ## Where is this used? What is it doing?
     def serialize(self):
         return {
             'id': self.id,
@@ -25,6 +28,7 @@ class Task():
 
 Tasks = {}
 
+## this is totally optional and just for demonstration purposes, remove if you wish
 testTask = Task("MAGICCCCCCCC")
 Tasks[testTask.id] = testTask
 
@@ -51,6 +55,9 @@ def addTask():
     Tasks[newTask.id] = newTask
     return getTasks()
 
+## this isn't actually ever used because our client code is missing the
+## code that sends a request to this route... do you want to try implementing
+## the client code (HTML/JavaScript)?!?!
 @app.route("/tasks/<int:taskId>", methods=["DELETE"])
 def removeTask(taskId):
     del Tasks[taskId]
@@ -66,5 +73,6 @@ def markUndone(taskId):
     Tasks[taskId].done = False
     return getTasks()
 
+## Actually start the server!!!
 if __name__ == "__main__":
     app.run(debug=True)
